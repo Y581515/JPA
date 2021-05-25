6c2af5335c9c06865346457536a8076130aa9487
yfess <yfess@DESKTOP-MO3P69F>
2021-05-25 19:26:45 +0200

    avstand

--- _Hello_DAT107/src/no/hvl/dat107/HelloDAT107.java
+++ _Hello_DAT107/src/no/hvl/dat107/HelloDAT107.java
@@ -1,18 +1,29 @@
 package no.hvl.dat107;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class HelloDAT107 {
-	
+
 	public static void main(String... blabla) {
 		System.out.println("Hello DAT107");
-		List<String> l= new ArrayList<>();;
+//		List<String> l = new ArrayList<>();
+//		;
+//
+//		l.add("en");
+//		l.add("en");
+//		System.out.println(l.size());
 		
-		l.add("en");
-		l.add("en");
-		System.out.println(l.size());
+		
+		System.out.println(finAvstandMellomPunkter(2,5,5,8));
+		
+	}
+
+	public static double finAvstandMellomPunkter(double x1, double y1, double x2, double y2) {
+		double xiAndre = Math.pow(x1 - x2, 2);
+		double yiAndre = Math.pow(y1 - y2, 2);
+
+		return Math.sqrt(xiAndre + yiAndre);
 	}
 
 }
